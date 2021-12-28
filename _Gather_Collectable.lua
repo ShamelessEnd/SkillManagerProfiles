ce91a8f5fef6b5513613df76fbb04486a8baddc7
ShamelessEnd <shamelessend@hotmail.com>
2021-12-28 19:19:58 +1100

    rdm

--- _Gather_Collectable.lua
+++ _Gather_Collectable.lua
@@ -68,18 +68,17 @@ local obj1 = {
 		[3] = {
 			["id"] = 22185;
 			["name"] = "Scrutiny";
-			["prio"] = 3;
+			["prio"] = 21;
 		};
 		[4] = {
-			["id"] = 22183;
-			["name"] = "Brazen Prospector";
-			["prio"] = 4;
+			["id"] = 232;
+			["name"] = "Solid Reason";
+			["prio"] = 3;
 		};
 		[5] = {
-			["collraritylt"] = 800;
-			["id"] = 22189;
-			["name"] = "Scrutiny";
-			["prio"] = 14;
+			["id"] = 232;
+			["name"] = "Solid Reason";
+			["prio"] = 4;
 		};
 		[6] = {
 			["id"] = 232;
@@ -87,45 +86,130 @@ local obj1 = {
 			["prio"] = 5;
 		};
 		[7] = {
-			["id"] = 215;
-			["name"] = "Ageless Words";
+			["id"] = 232;
+			["name"] = "Solid Reason";
 			["prio"] = 6;
 		};
 		[8] = {
-			["id"] = 215;
-			["name"] = "Ageless Words";
+			["id"] = 232;
+			["name"] = "Solid Reason";
 			["prio"] = 7;
 		};
 		[9] = {
-			["id"] = 26521;
-			["name"] = "Wise to the World";
+			["id"] = 215;
+			["name"] = "Ageless Words";
 			["prio"] = 8;
 		};
 		[10] = {
-			["id"] = 26522;
-			["name"] = "Wise to the World";
+			["id"] = 215;
+			["name"] = "Ageless Words";
 			["prio"] = 9;
 		};
 		[11] = {
-			["id"] = 26522;
-			["name"] = "Wise to the World";
+			["id"] = 215;
+			["name"] = "Ageless Words";
 			["prio"] = 10;
 		};
 		[12] = {
-			["collraritylt"] = 850;
-			["id"] = 22182;
-			["name"] = "Scour";
+			["id"] = 215;
+			["name"] = "Ageless Words";
 			["prio"] = 11;
 		};
 		[13] = {
-			["id"] = 22184;
-			["name"] = "Meticulous Prospector";
+			["id"] = 215;
+			["name"] = "Ageless Words";
 			["prio"] = 12;
 		};
 		[14] = {
-			["id"] = 22188;
-			["name"] = "Meticulous Woodsman";
+			["collraritygt"] = 604;
+			["gpbuff"] = "757";
+			["id"] = 22184;
+			["name"] = "Meticulous Prospector";
+			["prio"] = 22;
+			["singleuseonly"] = false;
+		};
+		[15] = {
+			["collraritygt"] = 554;
+			["gpbuff"] = "757";
+			["id"] = 22186;
+			["name"] = "Scour";
+			["prio"] = 23;
+			["singleuseonly"] = false;
+		};
+		[16] = {
+			["gpbuff"] = "757";
+			["id"] = 22183;
+			["name"] = "Brazen Prospector";
+			["prio"] = 24;
+			["singleuseonly"] = false;
+		};
+		[17] = {
+			["id"] = 26521;
+			["name"] = "Wise to the World";
 			["prio"] = 13;
+		};
+		[18] = {
+			["id"] = 26521;
+			["name"] = "Wise to the World";
+			["prio"] = 14;
+		};
+		[19] = {
+			["id"] = 26521;
+			["name"] = "Wise to the World";
+			["prio"] = 15;
+		};
+		[20] = {
+			["id"] = 26521;
+			["name"] = "Wise to the World";
+			["prio"] = 16;
+		};
+		[21] = {
+			["id"] = 26522;
+			["name"] = "Wise to the World";
+			["prio"] = 17;
+		};
+		[22] = {
+			["id"] = 26522;
+			["name"] = "Wise to the World";
+			["prio"] = 18;
+		};
+		[23] = {
+			["id"] = 26522;
+			["name"] = "Wise to the World";
+			["prio"] = 19;
+		};
+		[24] = {
+			["id"] = 26522;
+			["name"] = "Wise to the World";
+			["prio"] = 20;
+		};
+		[25] = {
+			["collraritylt"] = 800;
+			["gpnbuff"] = "757";
+			["id"] = 22185;
+			["name"] = "Scrutiny";
+			["prio"] = 25;
+			["singleuseonly"] = false;
+		};
+		[26] = {
+			["collraritygt"] = 850;
+			["id"] = 22184;
+			["name"] = "Meticulous Prospector";
+			["prio"] = 26;
+			["singleuseonly"] = false;
+		};
+		[27] = {
+			["collraritygt"] = 800;
+			["id"] = 22186;
+			["name"] = "Scour";
+			["prio"] = 27;
+			["singleuseonly"] = false;
+		};
+		[28] = {
+			["id"] = 22183;
+			["name"] = "Brazen Prospector";
+			["prio"] = 28;
+			["singleuseonly"] = false;
 		};
 	};
 	["version"] = 3;
