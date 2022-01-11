8eb25bedb4facbe9a753bf629e666aceef0a29b3
ShamelessEnd <shamelessend@hotmail.com>
2022-01-12 07:47:22 +1100

    Update tanks, add crafting

---
+++ _Craft_High.lua
@@ -0,0 +1,227 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["classes"] = {
+		[1] = false;
+		[2] = false;
+		[3] = false;
+		[4] = false;
+		[5] = false;
+		[6] = false;
+		[7] = false;
+		[8] = true;
+		[9] = true;
+		[10] = true;
+		[11] = true;
+		[12] = true;
+		[13] = true;
+		[14] = true;
+		[15] = true;
+		[16] = false;
+		[17] = false;
+		[18] = false;
+		[19] = false;
+		[20] = false;
+		[21] = false;
+		[22] = false;
+		[23] = false;
+		[24] = false;
+		[25] = false;
+		[26] = false;
+		[27] = false;
+		[28] = false;
+		[29] = false;
+		[30] = false;
+		[31] = false;
+		[32] = false;
+		[33] = false;
+		[34] = false;
+		[35] = false;
+		[36] = false;
+		[37] = false;
+		[38] = false;
+		[39] = false;
+		[40] = false;
+	};
+	["filters"] = {
+		[1] = "";
+		[2] = "";
+		[3] = "";
+		[4] = "";
+		[5] = "";
+	};
+	["mingp"] = 0;
+	["priosystem"] = false;
+	["skills"] = {
+		[1] = {
+			["id"] = 100386;
+			["name"] = "Muscle Memory";
+			["playerlevelmin"] = 54;
+			["prio"] = 1;
+			["type"] = 9;
+		};
+		[2] = {
+			["id"] = 4581;
+			["name"] = "Manipulation";
+			["playerlevelmin"] = 65;
+			["prio"] = 2;
+		};
+		[3] = {
+			["id"] = 19304;
+			["name"] = "Veneration";
+			["playerlevelmin"] = 15;
+			["prio"] = 3;
+		};
+		[4] = {
+			["id"] = 19003;
+			["name"] = "Waste Not II";
+			["playerlevelmin"] = 47;
+			["prio"] = 4;
+		};
+		[5] = {
+			["durabmin"] = 20;
+			["id"] = 100135;
+			["iqstackmax"] = 1;
+			["name"] = "Precise Touch";
+			["playerlevelmin"] = 53;
+			["prio"] = 5;
+			["type"] = 9;
+			["wn2stackmin"] = 1;
+		};
+		[6] = {
+			["id"] = 100410;
+			["name"] = "Groundwork";
+			["playerlevelmin"] = 72;
+			["prio"] = 6;
+			["type"] = 9;
+		};
+		[7] = {
+			["id"] = 100410;
+			["name"] = "Groundwork";
+			["playerlevelmin"] = 72;
+			["prio"] = 7;
+			["type"] = 9;
+		};
+		[8] = {
+			["durabmin"] = 20;
+			["id"] = 100106;
+			["iqstackmax"] = 1;
+			["name"] = "Basic Touch";
+			["playerlevelmin"] = 5;
+			["prio"] = 8;
+			["type"] = 9;
+			["wn2stackmin"] = 1;
+		};
+		[9] = {
+			["condition"] = "Excellent";
+			["id"] = 100306;
+			["name"] = "Preparatory Touch";
+			["playerlevelmin"] = 71;
+			["prio"] = 9;
+			["type"] = 9;
+			["wn2stackmin"] = 1;
+		};
+		[10] = {
+			["id"] = 19011;
+			["name"] = "Innovation";
+			["playerlevelmin"] = 26;
+			["prio"] = 10;
+		};
+		[11] = {
+			["id"] = 100306;
+			["name"] = "Preparatory Touch";
+			["playerlevelmin"] = 71;
+			["prio"] = 11;
+			["singleuseonly"] = false;
+			["type"] = 9;
+			["wn2stackmin"] = 1;
+		};
+		[12] = {
+			["condition"] = "Excellent";
+			["durabmin"] = 35;
+			["id"] = 100306;
+			["name"] = "Preparatory Touch";
+			["playerlevelmin"] = 71;
+			["prio"] = 13;
+			["qualitymaxper"] = 100;
+			["type"] = 9;
+		};
+		[13] = {
+			["durabmin"] = 35;
+			["id"] = 267;
+			["name"] = "Great Strides";
+			["playerlevelmin"] = 21;
+			["prio"] = 12;
+			["qualitymaxper"] = 100;
+		};
+		[14] = {
+			["id"] = 19011;
+			["name"] = "Innovation";
+			["playerlevelmin"] = 26;
+			["prio"] = 14;
+			["qualitymaxper"] = 100;
+		};
+		[15] = {
+			["durabmin"] = 35;
+			["id"] = 100306;
+			["name"] = "Preparatory Touch";
+			["playerlevelmin"] = 71;
+			["prio"] = 15;
+			["qualitymaxper"] = 100;
+			["type"] = 9;
+		};
+		[16] = {
+			["id"] = 100442;
+			["name"] = "Trained Finesse";
+			["playerlevelmin"] = 90;
+			["prio"] = 16;
+			["qualitymaxper"] = 100;
+			["type"] = 9;
+		};
+		[17] = {
+			["condition"] = "Excellent";
+			["id"] = 100346;
+			["iqstack"] = 10;
+			["name"] = "Byregot's Blessing";
+			["playerlevelmin"] = 50;
+			["prio"] = 17;
+			["qualitymaxper"] = 100;
+			["type"] = 9;
+		};
+		[18] = {
+			["id"] = 267;
+			["name"] = "Great Strides";
+			["playerlevelmin"] = 21;
+			["prio"] = 18;
+			["qualitymaxper"] = 100;
+		};
+		[19] = {
+			["id"] = 100346;
+			["iqstack"] = 10;
+			["name"] = "Byregot's Blessing";
+			["playerlevelmin"] = 50;
+			["prio"] = 19;
+			["qualitymaxper"] = 100;
+			["type"] = 9;
+		};
+		[20] = {
+			["id"] = 100210;
+			["name"] = "Careful Synthesis";
+			["playerlevelmin"] = 62;
+			["prio"] = 20;
+			["type"] = 9;
+		};
+		[21] = {
+			["id"] = 100105;
+			["name"] = "Basic Synthesis";
+			["playerlevelmin"] = 1;
+			["prio"] = 21;
+			["type"] = 9;
+		};
+	};
+	["update"] = 20191030;
+	["version"] = 3;
+}
+return obj1
---
+++ _Craft_Low.lua
@@ -0,0 +1,92 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["classes"] = {
+		[1] = false;
+		[2] = false;
+		[3] = false;
+		[4] = false;
+		[5] = false;
+		[6] = false;
+		[7] = false;
+		[8] = true;
+		[9] = true;
+		[10] = true;
+		[11] = true;
+		[12] = true;
+		[13] = true;
+		[14] = true;
+		[15] = true;
+		[16] = false;
+		[17] = false;
+		[18] = false;
+		[19] = false;
+		[20] = false;
+		[21] = false;
+		[22] = false;
+		[23] = false;
+		[24] = false;
+		[25] = false;
+		[26] = false;
+		[27] = false;
+		[28] = false;
+		[29] = false;
+		[30] = false;
+		[31] = false;
+		[32] = false;
+		[33] = false;
+		[34] = false;
+		[35] = false;
+		[36] = false;
+		[37] = false;
+		[38] = false;
+		[39] = false;
+		[40] = false;
+	};
+	["filters"] = {
+		[1] = "";
+		[2] = "";
+		[3] = "";
+		[4] = "";
+		[5] = "";
+	};
+	["mingp"] = 0;
+	["priosystem"] = false;
+	["skills"] = {
+		[1] = {
+			["id"] = 100290;
+			["name"] = "Trained Eye";
+			["playerlevelmin"] = 80;
+			["prio"] = 1;
+			["type"] = 9;
+		};
+		[2] = {
+			["id"] = 19304;
+			["maxprogrmin"] = 1200;
+			["name"] = "Veneration";
+			["playerlevelmin"] = 15;
+			["prio"] = 3;
+		};
+		[3] = {
+			["durabmin"] = 20;
+			["id"] = 100410;
+			["name"] = "Groundwork";
+			["playerlevelmin"] = 72;
+			["prio"] = 2;
+			["singleuseonly"] = false;
+			["type"] = 9;
+		};
+		[4] = {
+			["id"] = 100210;
+			["name"] = "Careful Synthesis";
+			["playerlevelmin"] = 62;
+			["prio"] = 4;
+			["type"] = 9;
+		};
+	};
+	["update"] = 20191030;
+	["version"] = 3;
+}
+return obj1
