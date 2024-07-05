6f92c52f76b7277ae95b6ea1eb12fbd06a913d1c
ShamelessEnd <shamelessend@hotmail.com>
2024-07-06 00:23:07 +1000

    Create _Battle_PCT.lua

---
+++ _Battle_PCT.lua
@@ -0,0 +1,405 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["classes"] = {
+		[1] = false;
+		[2] = false;
+		[3] = false;
+		[4] = false;
+		[5] = false;
+		[6] = false;
+		[7] = false;
+		[8] = false;
+		[9] = false;
+		[10] = false;
+		[11] = false;
+		[12] = false;
+		[13] = false;
+		[14] = false;
+		[15] = false;
+		[16] = false;
+		[17] = false;
+		[18] = false;
+		[19] = false;
+		[20] = false;
+		[21] = false;
+		[22] = false;
+		[23] = false;
+		[24] = false;
+		[25] = true;
+		[26] = false;
+		[27] = false;
+		[28] = false;
+		[29] = false;
+		[30] = false;
+		[31] = false;
+		[32] = false;
+		[33] = false;
+		[34] = false;
+		[35] = false;
+		[36] = false;
+		[37] = false;
+		[38] = false;
+		[39] = false;
+		[40] = false;
+		[41] = false;
+		[42] = true;
+	};
+	["filters"] = {
+		[1] = "";
+		[2] = "";
+		[3] = "";
+		[4] = "";
+		[5] = "";
+	};
+	["mingp"] = 0;
+	["priosystem"] = false;
+	["skills"] = {
+		[1] = {
+			["gcd"] = "False";
+			["id"] = 34680;
+			["ignoremoving"] = true;
+			["levelmin"] = 50;
+			["maxRange"] = 25;
+			["name"] = "Polishing Hammer";
+			["prio"] = 1;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[2] = {
+			["gcd"] = "False";
+			["id"] = 34679;
+			["ignoremoving"] = true;
+			["levelmin"] = 50;
+			["maxRange"] = 25;
+			["name"] = "Hammer Brush";
+			["prio"] = 2;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[3] = {
+			["gcd"] = "False";
+			["id"] = 34678;
+			["ignoremoving"] = true;
+			["levelmin"] = 50;
+			["maxRange"] = 25;
+			["name"] = "Hammer Stamp";
+			["prio"] = 3;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[4] = {
+			["gcd"] = "False";
+			["id"] = 34661;
+			["levelmin"] = 60;
+			["maxRange"] = 25;
+			["name"] = "Thunder II in Magenta";
+			["prio"] = 4;
+			["tarange"] = 5;
+			["tecenter"] = "Target";
+			["tecount"] = 3;
+			["terange"] = 6;
+		};
+		[5] = {
+			["gcd"] = "False";
+			["id"] = 34655;
+			["levelmin"] = 60;
+			["maxRange"] = 25;
+			["name"] = "Thunder in Magenta";
+			["prio"] = 5;
+		};
+		[6] = {
+			["gcd"] = "False";
+			["id"] = 34660;
+			["levelmin"] = 60;
+			["maxRange"] = 25;
+			["name"] = "Stone II in Yellow";
+			["prio"] = 6;
+			["tarange"] = 5;
+			["tecenter"] = "Target";
+			["tecount"] = 3;
+			["terange"] = 6;
+		};
+		[7] = {
+			["gcd"] = "False";
+			["id"] = 34654;
+			["levelmin"] = 60;
+			["maxRange"] = 25;
+			["name"] = "Stone in Yellow";
+			["prio"] = 7;
+		};
+		[8] = {
+			["gcd"] = "False";
+			["id"] = 34659;
+			["levelmin"] = 60;
+			["maxRange"] = 25;
+			["name"] = "Blizzard II in Cyan";
+			["prio"] = 8;
+			["tarange"] = 5;
+			["tecenter"] = "Target";
+			["tecount"] = 3;
+			["terange"] = 6;
+		};
+		[9] = {
+			["gcd"] = "False";
+			["id"] = 34653;
+			["levelmin"] = 60;
+			["maxRange"] = 25;
+			["name"] = "Blizzard in Cyan";
+			["prio"] = 9;
+		};
+		[10] = {
+			["gcd"] = "False";
+			["id"] = 34662;
+			["ignoremoving"] = true;
+			["levelmin"] = 80;
+			["maxRange"] = 25;
+			["name"] = "Holy in White";
+			["prio"] = 10;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[11] = {
+			["gcd"] = "False";
+			["id"] = 34669;
+			["levelmin"] = 70;
+			["maxRange"] = 0;
+			["name"] = "Starry Sky Motif";
+			["pnbuff"] = "3685+3674+3680";
+			["prio"] = 14;
+			["skncdtimemax"] = 20;
+			["sknoffcd"] = "34675";
+			["trg"] = "Player";
+		};
+		[12] = {
+			["gcd"] = "False";
+			["id"] = 34668;
+			["levelmin"] = 50;
+			["maxRange"] = 0;
+			["name"] = "Hammer Motif";
+			["pnbuff"] = "3685+3674+3680";
+			["prio"] = 13;
+			["skncdtimemax"] = 15;
+			["sknoffcd"] = "34674";
+			["trg"] = "Player";
+		};
+		[13] = {
+			["gauge2or"] = "0";
+			["gcd"] = "False";
+			["id"] = 34664;
+			["levelmin"] = 30;
+			["maxRange"] = 0;
+			["name"] = "Pom Motif";
+			["pnbuff"] = "3685+3674+3680+3675+3676";
+			["prio"] = 11;
+			["trg"] = "Player";
+		};
+		[14] = {
+			["gauge2or"] = "0";
+			["gcd"] = "False";
+			["id"] = 34665;
+			["levelmin"] = 30;
+			["maxRange"] = 0;
+			["name"] = "Wing Motif";
+			["pnbuff"] = "3685+3674+3680+3675+3676";
+			["prio"] = 12;
+			["trg"] = "Player";
+		};
+		[15] = {
+			["gcd"] = "False";
+			["id"] = 34658;
+			["levelmin"] = 25;
+			["maxRange"] = 25;
+			["name"] = "Water II in Blue";
+			["prio"] = 15;
+			["tarange"] = 5;
+			["tecenter"] = "Target";
+			["tecount"] = 3;
+			["terange"] = 6;
+		};
+		[16] = {
+			["gcd"] = "False";
+			["id"] = 34652;
+			["levelmin"] = 1;
+			["maxRange"] = 25;
+			["name"] = "Water in Blue";
+			["prio"] = 16;
+		};
+		[17] = {
+			["gcd"] = "False";
+			["id"] = 34657;
+			["levelmin"] = 25;
+			["maxRange"] = 25;
+			["name"] = "Aero II in Green";
+			["prio"] = 17;
+			["tarange"] = 5;
+			["tecenter"] = "Target";
+			["tecount"] = 3;
+			["terange"] = 6;
+		};
+		[18] = {
+			["gcd"] = "False";
+			["id"] = 34651;
+			["levelmin"] = 1;
+			["maxRange"] = 25;
+			["name"] = "Aero in Green";
+			["prio"] = 18;
+		};
+		[19] = {
+			["gcd"] = "False";
+			["id"] = 34656;
+			["levelmin"] = 25;
+			["maxRange"] = 25;
+			["name"] = "Fire II in Red";
+			["prio"] = 19;
+			["tarange"] = 5;
+			["tecenter"] = "Target";
+			["tecount"] = 3;
+			["terange"] = 6;
+		};
+		[20] = {
+			["gcd"] = "False";
+			["id"] = 34650;
+			["levelmin"] = 1;
+			["maxRange"] = 25;
+			["name"] = "Fire in Red";
+			["prio"] = 20;
+		};
+		[21] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 34674;
+			["levelmin"] = 50;
+			["maxRange"] = 0;
+			["name"] = "Striking Muse";
+			["prio"] = 21;
+			["trg"] = "Player";
+		};
+		[22] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 34675;
+			["levelmin"] = 70;
+			["maxRange"] = 0;
+			["name"] = "Starry Muse";
+			["prio"] = 22;
+			["tarange"] = 30;
+			["terange"] = 30;
+			["trg"] = "Player";
+		};
+		[23] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 34670;
+			["levelmin"] = 30;
+			["maxRange"] = 0;
+			["name"] = "Pom Muse";
+			["prio"] = 23;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[24] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 34671;
+			["levelmin"] = 30;
+			["maxRange"] = 0;
+			["name"] = "Winged Muse";
+			["prio"] = 24;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[25] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 34683;
+			["levelmin"] = 60;
+			["maxRange"] = 0;
+			["name"] = "Subtractive Palette";
+			["prio"] = 25;
+			["skncdtimemin"] = 15;
+			["sknoffcd"] = "34675";
+			["trg"] = "Player";
+		};
+		[26] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 34676;
+			["levelmin"] = 30;
+			["maxRange"] = 25;
+			["name"] = "Mog of the Ages";
+			["prio"] = 26;
+			["tarange"] = 25;
+			["terange"] = 25;
+		};
+		[27] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 7562;
+			["levelmin"] = 14;
+			["maxRange"] = 0;
+			["name"] = "Lucid Dreaming";
+			["prio"] = 27;
+			["trg"] = "Player";
+		};
+		[28] = {
+			["combat"] = "Out of Combat";
+			["gcd"] = "False";
+			["id"] = 34664;
+			["ignoremoving"] = true;
+			["levelmin"] = 30;
+			["maxRange"] = 0;
+			["name"] = "Pom Motif";
+			["prio"] = 28;
+			["trg"] = "Player";
+		};
+		[29] = {
+			["combat"] = "Out of Combat";
+			["gcd"] = "False";
+			["id"] = 34665;
+			["ignoremoving"] = true;
+			["levelmin"] = 30;
+			["maxRange"] = 0;
+			["name"] = "Wing Motif";
+			["prio"] = 29;
+			["trg"] = "Player";
+		};
+		[30] = {
+			["combat"] = "Out of Combat";
+			["gcd"] = "False";
+			["id"] = 34668;
+			["ignoremoving"] = true;
+			["levelmin"] = 50;
+			["maxRange"] = 0;
+			["name"] = "Hammer Motif";
+			["prio"] = 30;
+			["trg"] = "Player";
+		};
+		[31] = {
+			["combat"] = "Out of Combat";
+			["gcd"] = "False";
+			["id"] = 34669;
+			["ignoremoving"] = true;
+			["levelmin"] = 70;
+			["maxRange"] = 0;
+			["name"] = "Starry Sky Motif";
+			["prio"] = 31;
+			["trg"] = "Player";
+		};
+		[32] = {
+			["gcd"] = "True";
+			["gcdtimelt"] = 2;
+			["id"] = 34685;
+			["levelmin"] = 10;
+			["maxRange"] = 0;
+			["name"] = "Tempera Coat";
+			["phpb"] = 60;
+			["prio"] = 32;
+			["trg"] = "Player";
+		};
+	};
+	["version"] = 3;
+}
+return obj1
